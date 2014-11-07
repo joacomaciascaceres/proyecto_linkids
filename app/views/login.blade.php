58d74bd601889de2afb2bff242ba892a9698be37
Joaquin Macias <joaquin.macias.c@hotmail.com>
2014-11-06 21:27:23 -0300

    Login.blade 1.0.0
    Modifique la hoja de estilo

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,7 +1,44 @@
 <!DOCTYPE html>
 <html lang="es"><head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <title>Express Bus Packages v1.0 | Login | by ArielMax</title>
+        <title>Panel de Control | Proyecto LinKids</title>
+
+        <!--Hoja de Estilo-->
+        <style>
+        @import url(//fonts.googleapis.com/css?family=Lato:700);
+
+        body {
+            margin:0;
+            font-family:'Lato', sans-serif;
+            text-align:center;
+            color: #060606;
+            background-color: #b4c731;
+        }
+        .welcome {
+            width: 300px;
+            height: 200px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            margin-left: -150px;
+            margin-top: -100px;
+        }
+        h1 {
+            font-size: 32px;
+            margin: 16px 0 0 0;
+        }
+        h3 {
+            font-size: 15px;
+            margin: 8px 0 0 0;
+        }
+        footer{
+            font-size: 11px;
+            margin: 8px 0 0 0;
+            text-align: center;
+        }
+        </style>
+        <!--Fin Hoja de Estilo-->
+
 		<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="../../../../../Users/Administrador/Desktop/Plantilla_login-master/favicon.ico">
