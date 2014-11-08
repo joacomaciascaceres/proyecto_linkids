1a0bb27c570db0f558795bf7751ed4b4c64121d7
belaly <gladys.bta@gmail.com>
2014-11-07 22:10:39 -0300

    borre imagens, actualice login.blade
    borre unas imagenes que no servian para nada xD jaja y modifique el
    blade del login...ahora se ve mejor :)

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,77 +1,44 @@
 <!DOCTYPE html>
 <html lang="es"><head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <title>Panel de Control | Proyecto LinKids</title>
-
-        <!--Hoja de Estilo-->
-        <style>
-        @import url(//fonts.googleapis.com/css?family=Lato:700);
-
-        body {
-            margin:0;
-            font-family:'Lato', sans-serif;
-            text-align:center;
-            color: #060606;
-            background-color: #b4c731;
-        }
-        .welcome {
-            width: 300px;
-            height: 200px;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            margin-left: -150px;
-            margin-top: -100px;
-        }
-        h1 {
-            font-size: 32px;
-            margin: 16px 0 0 0;
-        }
-        h3 {
-            font-size: 15px;
-            margin: 8px 0 0 0;
-        }
-        footer{
-            font-size: 11px;
-            margin: 8px 0 0 0;
-            text-align: center;
-        }
-        </style>
-        <!--Fin Hoja de Estilo-->
-
+        <title>Ingreso de usuarios</title>
 		<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="../../../../../Users/Administrador/Desktop/Plantilla_login-master/favicon.ico">
-		<link rel="stylesheet" href="../../../../../Users/Administrador/Desktop/Plantilla_login-master/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../../../../../Users/Administrador/Desktop/Plantilla_login-master/font-awesome/css/font-awesome.css">
-        <link rel="stylesheet" href="../../../../../Users/Administrador/Desktop/Plantilla_login-master/css/login.css">
+        <link rel="shortcut icon" href="../../favicon.ico">
+		<link rel="stylesheet" href="../../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../font-awesome/css/font-awesome.css">
+         <?php echo HTML::style('../../css/login.css'); ?>
 	     </head>    
          <body>
         <div id="container">
             <div id="logo">
-                <img src="/img/kids.png" alt="" width="220" height="279">
+                <img src="{{ asset('../../img/kids.png') }}" alt="" width="21" height="236">
             </div>
             <div id="loginbox">            
-                <form id="loginform" action="../../../../../Users/Administrador/Desktop/Plantilla_login-master/index.html">
+                <form action="login" method="post">
     				<p>Introduzca usuario y contraseña para continuar.</p>
                     <div class="input-group input-sm">
-                        <span class="input-group-addon"><i class="fa fa-user"></i></span><input class="form-control" id="username" placeholder="Usuario" type="text">
+                        <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                      <input class="form-control" id="email" placeholder="Usuario" type="string" name="email"> 
                     </div>
                     <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-lock"></i></span><input class="form-control" id="password" placeholder="Contraseña" type="password">
+                        <span class="input-group-addon"><i class="fa fa-lock"></i></span><input class="form-control" id="password" placeholder="Contraseña" type="string" name="password">
                     </div>
                     <div class="form-actions clearfix">                      
  <input class="btn btn-block btn-primary btn-default" value="Entrar" type="submit">
                     </div>
                     <div class="footer-login">
+                        <div class="pull-left text">
+                             
+                        </div>
                         
-                        
-                  </div>
+                    </div>
                 </form>
                 </div>
         </div>
-        <script src="../../../../../Users/Administrador/Desktop/Plantilla_login-master/js/jquery.js"></script>  
-        <script src="../../../../../Users/Administrador/Desktop/Plantilla_login-master/js/jquery-ui.js"></script>
-        <script src="../../../../../Users/Administrador/Desktop/Plantilla_login-master/js/login.js"></script> 
+        
+        <?php echo HTML::script('../../js/jquery.js'); ?> 
+        <?php echo HTML::script('../../js/jquery-ui.js'); ?> 
+         <?php echo HTML::script('../../js/login.js'); ?>  
 </body>
 </html>
